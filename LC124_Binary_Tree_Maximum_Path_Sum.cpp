85dec898420337a1c238dcd03db622c7c4aa92d7
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-04-03 10:10:24 +0530

    Add LeetCode DSA Solutions In C++

---
+++ LC124_Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+private:
+    int res = INT_MIN;
+
+    int dfs(TreeNode* node)
+    {
+        if(!node)
+            return 0;
+        int leftGain = max(0, dfs(node -> left));
+        int rightGain = max(0, dfs(node -> right));
+
+        res = max(res, node -> val + leftGain + rightGain);
+
+        return node -> val + max(rightGain, leftGain);
+    }
+
+public:
+    int maxPathSum(TreeNode* root)
+    {
+        dfs(root);
+        return res;
+    }
+};
