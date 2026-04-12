b670d1358b1eec43d330aaafc2b21e12c76b3979
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-04-12 05:52:04 +0530

    Add LeetCode DSA Solutions In c++

---
+++ LC70_Climbing_Stairs.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int climbStairs(int n) 
+    {
+        if (n <= 2)
+            return n;
+
+        int a = 1, b = 2;
+        int c;
+
+        for(int i = 3; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
