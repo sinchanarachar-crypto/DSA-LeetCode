0f41ec55be1fa1f1ae1fc9295c4a623c0cd68f8e
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-04-17 19:07:57 +0530

    Add LeetCode DSA Solutions in c++

---
+++ LC208_Implement_Trie(Prefix_Tree).cpp
@@ -0,0 +1,62 @@
+struct TrieNode
+{
+    TrieNode* children[26] = {};  //Initialize all to nullptr
+    bool isEnd = false;
+};
+
+
+class Trie
+{
+private:
+    TrieNode* root;
+
+public:
+    Trie() : root(new TrieNode()) {}
+    
+    void insert(string word)
+    {
+        TrieNode* node = root;
+        for(char c : word)
+        {
+            int idx = c - 'a';
+            if(!node -> children[idx])
+                node -> children[idx] = new TrieNode();
+            node = node -> children[idx];
+        }
+        node -> isEnd = true;
+    }
+    
+    bool search(string word)
+    {
+        TrieNode* node = root;
+        for(char c : word)
+        {
+            int idx = c - 'a';
+            if(!node -> children[idx])
+                return false;
+            node = node -> children[idx];
+        }
+        return node -> isEnd;
+    }
+    
+    bool startsWith(string prefix)
+    {
+        TrieNode* node = root;
+        for(char c : prefix)
+        {
+            int idx = c - 'a';
+            if(!node -> children[idx])
+                return false;
+            node = node -> children[idx];
+        }
+        return true;        
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
