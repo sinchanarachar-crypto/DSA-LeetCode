389fd006f1c98fd611a3b47f44a5f1034e97bbc1
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-04-26 10:36:45 +0530

    Add LeetCode DSA solutions in c++

---
+++ LC739_Daily_Temperatures.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures)
+    {
+        int n = temperatures.size();
+        vector<int> answer(n, 0);
+        stack<int> st;
+
+        for(int i = 0; i < n; i++)
+        {
+            while(!st.empty() && temperatures[i] > temperatures[st.top()])
+            {
+                int j = st.top();
+                st.pop();
+                answer[j] = i - j;
+            }
+            st.push(i);
+        }
+        return answer;
+    }
+};
