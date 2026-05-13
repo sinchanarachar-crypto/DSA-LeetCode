312426e8fb36f5e910a244057a605006c2cc14ff
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-05-13 23:15:53 +0530

    Add LeetCode DSA Solutions in c++

--- LC448_Find_All_Numbers_Disappeared_In_An_Array.cpp
+++ LC448_Find_All_Numbers_Disappeared_In_An_Array.cpp
@@ -3,22 +3,22 @@ class Solution
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums)
     {
-       vector<int> result;
+        vector<int> result;
 
-       for(int i = 0; i < nums.size(); i++)
-       {
+        for(int i = 0; i < nums.size(); i++)
+        {
             int index = abs(nums[i]) - 1;
 
             if(nums[index] > 0)
                 nums[index] = -nums[index];
-       }
+        }
 
-       for(int i = 0; i < nums.size(); i++)
-       {
+        for(int i = 0; i < nums.size(); i++)
+        {
             if(nums[i] > 0)
                 result.push_back(i + 1);
-       }
+        }
 
-       return result;
+        return result;
     }
 };
