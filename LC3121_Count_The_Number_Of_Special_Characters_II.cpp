ce0c14dd8c7e2627553a6d914c456952f963ecf7
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-05-27 07:28:33 +0530

    Add LeetCode DSA Solutions in c++

---
+++ LC3121_Count_The_Number_Of_Special_Characters_II.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int numberOfSpecialChars(string word)
+    {
+        vector<int> lower(26, -1);
+        vector<int> upper(26, -1);
+
+        for(int i = 0; i < word.size(); i++)
+        {
+            char ch = word[i];
+            if(islower(ch))
+                lower[ch - 'a'] = i;
+            else if(upper[ch - 'A'] == -1)
+                upper[ch - 'A'] = i;
+        }
+        int res = 0;
+        for(int ch = 0; ch < 26; ch++)
+        {
+            if(lower[ch] != -1 && upper[ch] != -1 && upper[ch] > lower[ch])
+                res++;
+        }
+        return res;
+    }
+};
