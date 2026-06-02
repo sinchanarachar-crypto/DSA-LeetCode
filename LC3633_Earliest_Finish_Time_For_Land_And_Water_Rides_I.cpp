2dce227d7427752a4a848f6cd080b9f3b3a34cc2
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-06-02 08:31:36 +0530

    Add LeetCode DSA Solutions in c++

---
+++ LC3633_Earliest_Finish_Time_For_Land_And_Water_Rides_I.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    int earliestFinishTime(vector<int>& landStartTime,
+                           vector<int>& landDuration,
+                           vector<int>& waterStartTime,
+                           vector<int>& waterDuration)
+    {
+        long long ans = LLONG_MAX;
+
+        int n = landStartTime.size();
+        int m = waterStartTime.size();
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                // Land -> Water
+                long long landFinish =
+                    (long long)landStartTime[i] + landDuration[i];
+
+                long long finish1 =
+                    max(landFinish,
+                        (long long)waterStartTime[j])
+                    + waterDuration[j];
+
+                ans = min(ans, finish1);
+
+                // Water -> Land
+                long long waterFinish =
+                    (long long)waterStartTime[j] + waterDuration[j];
+
+                long long finish2 =
+                    max(waterFinish,
+                        (long long)landStartTime[i])
+                    + landDuration[i];
+
+                ans = min(ans, finish2);
+            }
+        }
+
+        return (int)ans;
+    }
+};
