1dea445b9013b4d6c2dbc2c65fa74e981dc077fd
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-07-22 07:52:01 +0530

    Add LeetCode DSA Solutiolns in c++

---
+++ LC1171_Remove_Zero_Sum_Consecutive_Nodes_From_Linked_List.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode* removeZeroSumSublists(ListNode* head)
+    {
+        int prefixSum = 0;
+        unordered_map<int, ListNode*> mp;
+
+        ListNode* dummy = new ListNode(0);
+
+        dummy -> next = head;
+
+        mp[0] = dummy;
+
+        while(head != NULL)
+        {
+            prefixSum += head -> val;
+
+            if(mp.find(prefixSum) != mp.end())
+            {
+                ListNode* start = mp[prefixSum];
+                ListNode* temp = start;
+
+                int psum = prefixSum;
+
+                while(temp != head)
+                {
+                    temp = temp -> next;
+                    psum += temp -> val;
+
+                    if(temp != head)
+                        mp.erase(psum);
+                }
+
+                start -> next = head -> next;
+
+            }
+
+            else
+                mp[prefixSum] = head;
+
+            head = head -> next;
+        }
+
+        return dummy -> next;
+    }
+};
