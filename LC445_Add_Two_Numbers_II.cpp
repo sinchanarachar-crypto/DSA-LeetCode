abfbfab9efb372c95dd0d082daa84bf75237ba3c
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-07-26 14:36:37 +0530

    Add LeetCode DSA Solutions in c++

---
+++ LC445_Add_Two_Numbers_II.cpp
@@ -0,0 +1,105 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+
+    /*ListNode* reverseLL(ListNode* head)
+    {
+        if(!head || !head -> next) return head;
+
+        ListNode* last = reverseLL(head -> next);
+        head -> next -> next = head;
+        head -> next = NULL;
+        return last;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+    {
+        l1 = reverseLL(l1);
+        l2 = reverseLL(l2);
+
+        int sum = 0;
+        int carry = 0;
+        ListNode* ans = new ListNode();
+
+        while(l1 != NULL ||l2 != NULL)
+        {
+            if(l1)
+            {
+                sum += l1 -> val;
+                l1 = l1 -> next;
+            }
+            if(l2)
+            {
+                sum += l2 -> val;
+                l2 = l2 -> next;
+            }
+
+            ans -> val = sum % 10;
+            carry = sum/10;
+
+            ListNode* newNode = new ListNode(carry);
+            newNode -> next = ans;
+            ans = newNode;
+            sum = carry;
+        }
+
+        return carry == 0? ans -> next : ans;
+    }*/
+
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+    {
+        stack<int> s1;
+        stack<int> s2;
+
+        while(l1 != NULL)
+        {
+            s1.push(l1 -> val);
+            l1 = l1 -> next;
+        }
+
+        while(l2 != NULL)
+        {
+            s2.push(l2 -> val);
+            l2 = l2 -> next;
+        }
+
+        int sum = 0;
+        int carry = 0;
+        ListNode* ans = new ListNode();
+
+        while(!s1.empty() || !s2.empty())
+        {
+            if(!s1.empty())
+            {
+                sum += s1.top();
+                s1.pop();
+            }
+            if(!s2.empty())
+            {
+                sum += s2.top();
+                s2.pop();
+            }
+
+            ans -> val = sum % 10;
+            carry = sum/10;
+
+            ListNode* newNode = new ListNode(carry);
+            newNode -> next = ans;
+            ans = newNode;
+            sum = carry;
+        }
+
+        return carry == 0? ans -> next : ans;
+    }
+};
