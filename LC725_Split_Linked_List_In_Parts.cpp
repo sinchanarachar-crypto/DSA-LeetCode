f341674a9960e1d455407d16ab912a5793eba66c
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-07-29 12:29:27 +0530

    Add LeetCode DSA Solutions in c++

---
+++ LC725_Split_Linked_List_In_Parts.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k)
+    {
+        //Length of list
+        ListNode* curr = head;
+        int L = 0;
+
+        while(curr)
+        {
+            L++;
+            curr = curr -> next;
+        }
+
+        int eachBucketNodes = L / k;
+        int remainderNodes = L % k;
+
+        vector<ListNode*> result(k, NULL);
+        ListNode* prev = NULL;
+
+        curr = head;
+
+        //Creating Array
+        for(int i = 0; i < k; i++)
+        {
+            result[i] = curr;
+            for(int count = 1; count <= eachBucketNodes + (remainderNodes > 0? 1 : 0); count++)
+            {
+                prev = curr;
+                curr = curr -> next;
+            }
+
+            if (prev)
+                prev -> next = nullptr;
+
+            remainderNodes--;
+        }
+
+        return result;
+    }
+};
