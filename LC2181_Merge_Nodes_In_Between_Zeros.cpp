b2797fb94b76699940af2b61e55ab198b737fc2b
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-08-07 17:52:17 +0530

    Add LeetCode DSA Solutions in c++

---
+++ LC2181_Merge_Nodes_In_Between_Zeros.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode* mergeNodes(ListNode* head)
+    /*{
+        ListNode* p1 = head -> next;
+        ListNode* p2 = p1;
+
+        while(p2 != NULL)
+        {
+            int sum = 0;
+            while(p2 != NULL && p2 -> val != 0)
+            {
+                sum += p2 -> val;
+                p2 = p2 -> next;
+            }
+
+            p1 -> val = sum;
+            p2 = p2 -> next;
+            p1 -> next = p2;
+            p1 = p1 -> next;
+        }
+
+        return head -> next;
+    }*/
+
+    {
+        head = head -> next;
+        if(head == NULL) return head;
+
+        ListNode* temp = head;
+        int sum = 0;
+        while(temp != NULL && temp -> val != 0)
+        {
+            sum += temp -> val;
+            temp = temp -> next;
+        }
+
+        head -> val = sum;
+        head -> next = mergeNodes(temp);
+
+        return head;
+    }
+};
