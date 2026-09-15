57500902331dec8914cbe948a6229a91b062765b
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-09-15 21:32:02 +0530

    Add LeetCode DSA Solutions in c++

--- LC1512_Number_Of_Good_Pairs.cpp
+++ LC1512_Number_Of_Good_Pairs.cpp
@@ -3,48 +3,55 @@ class Solution
 public:
     int numIdenticalPairs(vector<int>& nums)
     /*{
-        int n = nums.size();
         int result = 0;
+        int n = nums.size();
 
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < n - 1; i++)
         {
             for(int j = i + 1; j < n; j++)
-                if(nums[i] == nums[j]) result++;
+            {
+                if(nums[i] == nums[j])
+                {
+                    result++;
+                }
+            }
         }
 
         return result;
-        
     }*/
 
     /*{
         int n = nums.size();
-        int result = 0;
-
         unordered_map<int, int> mp;
 
-        for(auto &num : nums) mp[num]++;
+        for(int &num : nums)
+        {
+            mp[num]++;
+        }
+
+        int result = 0;
 
         for(auto &it : mp)
         {
             int count = it.second;
-            
-            result += (count * (count - 1)) / 2;
+
+            result += count*(count - 1) / 2;
         }
-        return result;
+
+        return  result;
     }*/
 
     {
         int n = nums.size();
         int result = 0;
-
         unordered_map<int, int> mp;
 
-        for(auto &num : nums)
+        for(int &num : nums)
         {
             result += mp[num];
-
             mp[num]++;
         }
+
         return result;
     }
 };
