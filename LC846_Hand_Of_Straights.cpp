3d86600c71bc40181a9fd906ae3ae02221ccf6ec
Sinchana Achar <sinchanarachar.crypto@gmail.com>
2026-09-22 23:26:40 +0530

    Add LeetCode DSA Solutions in c++

---
+++ LC846_Hand_Of_Straights.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize)
+    {
+        int n = hand.size();
+
+        if(n % groupSize != 0) return false;
+
+        map<int, int> mp;
+
+        for(int card : hand)
+        {
+            mp[card]++;
+        }
+
+        while(!mp.empty())
+        {
+            int curr = mp.begin() -> first;
+
+            for(int i = 0; i < groupSize; i++)
+            {
+                if(mp[curr + i] == 0) return false;
+            
+                mp[curr + i]--;
+
+                if(mp[curr + i] < 1)
+                {
+                   mp.erase(curr + i);
+                }
+            }
+        }
+
+        return true;
+    }
+};
